24bc58a1ddcdcd830dbc77e0b6c302533126bb8d
Aliwka700 <ali_akhmet@kbtu.kz>
2025-03-19 15:58:15 +0500

    Update music.py

--- music.py
+++ music.py
@@ -1,16 +1,16 @@
 import pygame
 import os
 
-# Инициализация Pygame
+
 pygame.init()
 pygame.mixer.init()
 
-# Список музыкальных файлов
+
 music_files = ["song1.mp3", "song2.mp3", "song3.mp3"]
 current_track = 0
 pygame.mixer.music.load(music_files[current_track])
 
-# Окно
+
 WIDTH, HEIGHT = 300, 200
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Music Player")
@@ -24,15 +24,15 @@ while running:
             running = False
     
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_p]:  # Play
+    if keys[pygame.K_p]: 
         pygame.mixer.music.play()
-    if keys[pygame.K_s]:  # Stop
+    if keys[pygame.K_s]:  
         pygame.mixer.music.stop()
-    if keys[pygame.K_n]:  # Next track
+    if keys[pygame.K_n]:  
         current_track = (current_track + 1) % len(music_files)
         pygame.mixer.music.load(music_files[current_track])
         pygame.mixer.music.play()
-    if keys[pygame.K_b]:  # Previous track
+    if keys[pygame.K_b]:  
         current_track = (current_track - 1) % len(music_files)
         pygame.mixer.music.load(music_files[current_track])
         pygame.mixer.music.play()
